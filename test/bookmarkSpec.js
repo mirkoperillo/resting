0bb8615a3491a481d7d1b3b2eca01a78c00162fd
Mario A. Santini <alexmario74@gmail.com>
2018-10-19 10:55:32 +0200

    added tests for replaceBookmark

--- test/bookmarkSpec.js
+++ test/bookmarkSpec.js
@@ -14,3 +14,70 @@ describe("Save bookmarks", function() {
     expect('OK').toBe(response.result);
   });
 });
+
+describe("Handling bookmarks", function() {
+  const bookmarkProvider = makeBookmarkProvider(mockStorageProvider);
+
+  describe('replaceBookmark should', function() {
+    it("add a bookmark if it not exists", function() {
+      const startFolder = bookmarkProvider.makeFolder(1, "my folder");
+      const newBookmark = bookmarkProvider.makeBookmark(1, "request", "bookmark name", null);
+      const updatedFolder = bookmarkProvider.replaceBookmark(startFolder, newBookmark);
+      expect(updatedFolder.id).toBe(startFolder.id);
+      expect(updatedFolder.name).toBe(startFolder.name);
+      expect(updatedFolder.bookmarks.length).toBe(1);
+      expect(startFolder.bookmarks.length).toBe(0);
+  
+      const otherNewBookmark = bookmarkProvider.makeBookmark(2, "another request", "bookmark name", null);
+      const leastFolder = bookmarkProvider.replaceBookmark(updatedFolder, otherNewBookmark);
+      expect(leastFolder.bookmarks.length).toBe(2);
+    });
+  
+    it("replace an existing bookmark", function() {
+      const startFolder = bookmarkProvider.makeFolder(1, "my folder", [
+        bookmarkProvider.makeBookmark(1, "request 1", "bookmark name", null)
+      ]);
+      const emendedBookmark = bookmarkProvider.makeBookmark(1, "request 2", "bookmark name", null);
+  
+      const updatedFolder = bookmarkProvider.replaceBookmark(startFolder, emendedBookmark);
+  
+      expect(updatedFolder.bookmarks.length).toBe(1);
+      expect(updatedFolder.bookmarks[0].name).toBe(emendedBookmark.name);
+      expect(updatedFolder.bookmarks[0].request).toBe(emendedBookmark.request);
+      expect(startFolder.bookmarks[0].request).not.toBe(emendedBookmark.request);
+    });
+
+    it("replace the first bookmark", function() {
+      const startFolder = bookmarkProvider.makeFolder(1, "my folder", [
+        bookmarkProvider.makeBookmark(1, "request POST", "bookmark name", null),
+        bookmarkProvider.makeBookmark(2, "request GET", "another name", null),
+        bookmarkProvider.makeBookmark(3, "request PUT", "last name", null)
+      ]);
+      const emendedBookmark = bookmarkProvider.makeBookmark(1, "request DELETE", "bookmark name", null);
+  
+      const updatedFolder = bookmarkProvider.replaceBookmark(startFolder, emendedBookmark);
+  
+      expect(updatedFolder.bookmarks.length).toBe(3);
+      expect(updatedFolder.bookmarks[0].name).toBe(emendedBookmark.name);
+      expect(updatedFolder.bookmarks[0].request).toBe(emendedBookmark.request);
+      expect(startFolder.bookmarks[0].request).not.toBe(emendedBookmark.request);
+    });
+
+    it("replace the last bookmark", function() {
+      const startFolder = bookmarkProvider.makeFolder(1, "my folder", [
+        bookmarkProvider.makeBookmark(1, "request POST", "bookmark name", null),
+        bookmarkProvider.makeBookmark(2, "request GET", "another name", null),
+        bookmarkProvider.makeBookmark(3, "request PUT", "last name", null)
+      ]);
+      const emendedBookmark = bookmarkProvider.makeBookmark(3, "request DELETE", "bookmark name", null);
+  
+      const updatedFolder = bookmarkProvider.replaceBookmark(startFolder, emendedBookmark);
+  
+      expect(updatedFolder.bookmarks.length).toBe(3);
+      const lastBookmark = updatedFolder.bookmarks[2];
+      expect(lastBookmark.name).toBe(emendedBookmark.name);
+      expect(lastBookmark.request).toBe(emendedBookmark.request);
+      expect(startFolder.bookmarks[2].request).not.toBe(emendedBookmark.request);
+    });
+  });
+});
