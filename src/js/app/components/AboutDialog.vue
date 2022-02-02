3ed5eeb67eefbca9e9fe081161fbddeb65662dca
mirko <mirko.perillo@gmail.com>
2022-02-02 07:44:46 +0100

    Bump version

--- src/js/app/components/AboutDialog.vue
+++ src/js/app/components/AboutDialog.vue
@@ -1,6 +1,6 @@
 <template>
   <r-dialog title="About Resting" @dismiss-dialog="$emit('dismiss-dialog')">
-    <h4>Resting v1.3.0</h4>
+    <h4>Resting v1.3.1</h4>
     <p>License: <a href="https://github.com/mirkoperillo/resting/blob/master/LICENSE" target="_blank">GPLv3</a></p>
     <a target="_blank" href="https://github.com/mirkoperillo/resting">Project Website</a> --
     <a target="_blank" href="https://github.com/mirkoperillo/resting/issues">Issue tracker</a>
