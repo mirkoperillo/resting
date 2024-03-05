3e38f8b9c2a6eee56d3a0102cd6f8d052aaa7224
mirko <mirko.perillo@gmail.com>
2024-03-05 11:32:46 +0100

    Support manifest v3 for Chrome (#197)
    * Support manifest v3
    
    * Fix deps

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+cd $parent_path;
+echo "Moved to scripts dir"
+
+if [ ! -d "../dist" ] 
+then
+  mkdir ../dist
+fi
 
 if [ ! -d "../dist" ] 
 then
@@ -17,4 +27,15 @@ echo "vue components built"
 cd src
 zip -r ../dist/resting.zip *
 cd ..
+if [ $1 == 'chrome' ]
+then
+  echo "copy chrome manifest"
+  zip -j dist/resting.zip addon/chrome/*
+else
+  echo "copy firefox manifest"
+  zip -j dist/resting.zip addon/firefox/*
+fi
+
 zip dist/resting.zip LICENSE README.md
+
+echo "Done!"
--- vue.config.js
+++ vue.config.js
@@ -5,13 +5,13 @@ module.exports = {
     resolve: {
       alias: {
         Components: path.resolve(__dirname, 'src/js/app/components'),
-        Services: path.resolve(__dirname, 'src/js/app')
-      }
+        Services: path.resolve(__dirname, 'src/js/app'),
+      },
     },
     externals: {
       'Services/clipboard': 'app/clipboard',
       'Services/storage': 'app/storage',
-      'Services/bacheca': 'app/bacheca'
-    }      
-  }
+      'Services/bacheca': 'app/bacheca',
+    },
+  },
 }
